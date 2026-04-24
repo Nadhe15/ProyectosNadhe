a860a90c8eded541d50679e2ba3a6cefbf973240
Nadheska <nadheska0202@gmail.com>
2026-04-23 23:23:19 -0400

    Adivina el número, completo

--- AdivinaNum.py
+++ AdivinaNum.py
@@ -6,5 +6,10 @@ dato = random.randint(1,100)
 
 while intento != dato:
     intento = int(input("Adivine un número del 1 al 100: "))
+    if dato > intento:
+      print("Pista: Intenta un número más alto ")
+    elif dato < intento: 
+       print("Pista: Intenta un número más bajo ")
+
 print("Muy bien, adivinaste!")
 
